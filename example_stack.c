75ae868c879da40c803a5f0c1ec70ef1d5f3c950
Matt <pengcheng_xu@yeah.net>
2018-03-29 23:29:49 +0800

    Create example_stack.c

---
+++ example_stack.c
@@ -0,0 +1,34 @@
+/****************************************************************************************
+
+*****************************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+//结构体定义
+typedef struct
+{
+    int *base;
+    int *top;
+    int stacksize;
+}Stack_Op;
+
+//栈的初始化
+int Stack_Init()
+{
+    return TRUE;
+}
+
+//入栈操作
+int StackPush_Process()
+{
+    return TRUE;
+}
+
+//出栈操作
+int StackPop_Process()
+{
+    return TRUE;
+}
+
+//
