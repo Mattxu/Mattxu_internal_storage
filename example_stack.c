f001cf4cf8a9f25f31ae8af3cfaa14679f1f1342
Matt <pengcheng_xu@yeah.net>
2018-03-30 23:49:23 +0800

    Update example_stack.c

--- example_stack.c
+++ example_stack.c
@@ -5,6 +5,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define TRUE    (1)
+#define FALSE   (0)
+#define STACK_SIZE (100)
+
 //结构体定义
 typedef struct
 {
@@ -14,8 +18,16 @@ typedef struct
 }Stack_Op;
 
 //栈的初始化
-int Stack_Init()
+int Stack_Init(Stack_Op *pstack)
 {
+    pstack->base = (int *)malloc(STACK_SIZE * sizeof(int));
+    if(NULL == pstack->base)
+    {
+        return FALSE;
+    }
+    pstack->top = pstack->base;     //top指针指向栈底，该栈处于初始化
+    pstack->stacksize = STACK_SIZE;
+    
     return TRUE;
 }
 
