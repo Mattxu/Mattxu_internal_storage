47570cc8634e805725d00c622c515ff8325b8910
Matt <pengcheng_xu@yeah.net>
2018-04-01 23:21:02 +0800

    Update example_stack.c

--- example_stack.c
+++ example_stack.c
@@ -13,59 +13,116 @@
 //结构体定义
 typedef struct
 {
-    int *base;
-    int *top;
-    int stacksize;
+    int *base;        //栈底指针
+    int *top;         //栈顶指针
+    int stacksize;    //栈的大小
 }Stack_Op;
 
 //栈的初始化
-int Stack_Init(Stack_Op *pstack)
+int Stack_Init(Stack_Op *ptstack)
 {
-    pstack->base = (int *)malloc(STACK_SIZE * sizeof(int));
-    if(NULL == pstack->base)
+    ptstack->base = (int *)malloc(STACK_SIZE * sizeof(int));    //申请栈的空间
+    if(NULL == ptstack->base)
     {
         return FALSE;
     }
-    pstack->top = pstack->base;     //top指针指向栈底，该栈处于初始化
-    pstack->stacksize = STACK_SIZE;
+    ptstack->top = ptstack->base;     //top指针指向栈底，该栈处于初始化
+    ptstack->stacksize = STACK_SIZE;
     
     return TRUE;
 }
 
 //入栈操作
-int StackPush_Process(Stack_Op *pstack, int item)
+int StackPush_Process(Stack_Op *ptstack, int item)
 {
-    if((pstack->top - pstack->base) >= pstack->stacksize)
+    if((ptstack->top - ptstack->base) >= ptstack->stacksize)    //检查申请的栈的大小
     {
-        pstack->base = (int *)realloc(pstack->base, (pstack->stacksize + STACK_SIZE_ADD) * sizeof(int));
+        ptstack->base = (int *)realloc(ptstack->base, (ptstack->stacksize + STACK_SIZE_ADD) * sizeof(int));
 
-        if(NULL == pstack->base)
+        if(NULL == ptstack->base)
         {
             return FALSE;
         }
-        pstack->top = pstack->base + pstack->stacksize;
-        pstack->stackzie += STACK_SIZE_ADD;
+        ptstack->top = ptstack->base + ptstack->stacksize;
+        ptstack->stackzie += STACK_SIZE_ADD;
     }
-    *(pstack->top++) = item;
+    *(ptstack->top++) = item;    //给申请的栈空间进行依次赋值，先赋值，栈顶指针再累加
     
     return TRUE;
 }
 
 //出栈操作
-int StackPop_Process(Stack_Op *pstack， int &item)
+int StackPop_Process(Stack_Op *ptstack， int &item)
 {
-    if(pstack->top == pstack->base)    //栈中没有内容
+    if(ptstack->top == ptstack->base)    //栈中没有内容
     {
         return FALSE;
     }
-    item = *(--pstack->top);
+    item = *(--ptstack->top);    //依次出栈操作，栈顶指针先递减，再出栈
         
     return TRUE;
 }
 
-//栈的删除
-int StackDel_Process()
+//获取栈顶元素
+int StackGetTop_Process(Stack_Op *ptstack, int &item)
 {
+    if(ptstack->top == ptstack->base)
+    {
+        return FALSE;
+    }
+    item = *(ptstack->top - 1);    //获取栈顶元素
+    
+    return TRUE;
+}
+
+//判断栈是否为空
+int StackEmpty_Process(Stack_Op *ptstack)
+{
+    if(ptstack->top == ptstack->base)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+//栈的销毁
+int StackDel_Process(Stack_Op *ptstack)
+{
+    ptstack->top = ptstack->base;
+    free(ptstack->base);
+    ptstack->stacksize = STACK_SIZE;
+    
+    return TRUE;
+}
+
+//main.c实现入栈、出栈的基本操作
+int main()
+{
+    Stack_Op *ptstack;
+    int push_num;
+    int push_val;
+    
+    /* 初始化栈 */
+    Stack_Init(ptstack);
+    printf("The size of stack is %d\n", &pstack->stacksize);
+    
+    /* 根据输入进行栈的填充 */
+    printf("Please enter the number to push into stack: ");
+    scanf("%d\n", &push_num);
+    
+    while(push_num--)
+    {
+        scanf("%d\n", &pushval);
+        StackPush_Process(ptstack, push_val);
+        printf("The size after push value:%d,%d\n", push_val, ptstack->stacksize);
+    }
+    
+    //获取栈顶元素
+    StackGetTop_Process(ptstack, push_val);
+    printf("The top value is: %d\n", push_val);
     
     return TRUE;
 }
